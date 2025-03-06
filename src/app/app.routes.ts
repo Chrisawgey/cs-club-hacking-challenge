5ea49ac126073b5b5b1897a1066129b072c92b08
chrisawgey <chrisvpopoca@gmail.com>
2025-03-06 13:52:00 -0500

    log in

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { EventsComponent } from './components/events/events.component';
+import { MembersComponent } from './components/members/members.component';
+import { ChallengesComponent } from './components/challenges/challenges.component';
+import { LoginComponent } from './components/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'events', component: EventsComponent },
+  { path: 'members', component: MembersComponent },
+  { path: 'challenges', component: ChallengesComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '**', redirectTo: '' }
+];
---
+++ src/app/components/events/events.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-events',
+  standalone: true,
+  imports: [],
+  templateUrl: './events.component.html',
+  styleUrls: ['./events.component.css']
+})
+export class EventsComponent {
+
+}
